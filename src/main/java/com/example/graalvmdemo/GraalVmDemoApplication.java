934d90f024827f19151f28ebfebc1d6e4b403af4
includeno <includeno@gmail.com>
2023-04-15 17:31:01 +0800

    init

---
+++ src/main/java/com/example/graalvmdemo/GraalVmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.graalvmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalVmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraalVmDemoApplication.class, args);
+    }
+
+}
